9f12afb223b7df890b23534de28db9ba4202d857
joyhossain1542 <mj8969043@gmail.com>
2024-04-16 15:05:54 +0600

    first commit

---
+++ New folder/index.js
@@ -0,0 +1,7 @@
+// let id1 = document.getElementById("id1");
+
+// id1.innerHTML = "Hi hablu programmer";  
+
+let classname1 = document.getElementsByClassName("classname1")[0];
+
+classname1.innerHTML ="subscribe Hablu"
---
+++ README.md
@@ -0,0 +1,1 @@
+# joyhossain1542
